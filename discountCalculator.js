02bc0fc7a56fd4566258b1eec3576cbdc0d74f1a
JosephYounes <joseph.younes52@hotmail.com>
2025-05-22 17:18:52 -0400

    this is my debugging lab

--- discountCalculator.js
+++ discountCalculator.js
@@ -1,17 +1,9 @@
 function calculateDiscountedPrice(quantity, pricePerItem) {
-    let totalPrice = 0;
     debugger
-    for (let i = 1; i < quantity; i++) {
-        totalPrice += pricePerItem;
-    }
-
-    if (quantity >= 10) {
+    let totalPrice = quantity * pricePerItem;
+    if (quantity > 10) {
         totalPrice *= 0.9;
     }
-
     return totalPrice;
 }
-
-
-
 module.exports = calculateDiscountedPrice;
